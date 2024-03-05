c307a1b462853fe6a3178eeaabc6271924c8006f
Brian Zoetewey <brian.zoetewey@cru.org>
2024-03-05 17:02:18 -0500

    Initial commit

---
+++ foo/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "cru-tf-remote-state"
+    dynamodb_table = "terraform-state-lock"
+    region         = "us-east-1"
+    key            = "spacelift-test/foo/foo/terraform.tfstate"
+  }
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+  }
+  required_version = "~> 1.6.4"
+}
