69eb7e40e8eedd0aab3869c7b92fcad109268fb7
Brian Zoetewey <brian.zoetewey@cru.org>
2024-03-14 11:11:50 -0400

    Mount file to context instead of stack

--- foo/dolor/prod/spacelift.tf
+++ foo/dolor/prod/spacelift.tf
@@ -1,11 +1,10 @@
-# Lookup Ansible stack
-data "spacelift_stack" "ansible" {
-  stack_id = "spacelift-ansible"
+data "spacelift_context" "ansible" {
+  context_id = "ansible"
 }
 
 # Add mounted file to ansible stack
 resource "spacelift_mounted_file" "private_key" {
-  stack_id      = data.spacelift_stack.ansible.id
+  context_id    = data.spacelift_context.ansible.id
   relative_path = "id_rsa"
   content       = base64encode(tls_private_key.private_key.private_key_pem)
 }
